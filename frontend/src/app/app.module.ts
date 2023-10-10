91be09063254d6d977f83c6ee8313ce2b0f848ed
aaeekaayyyyyy <ayushmankhandelwal04@gmail.com>
2023-10-10 21:38:12 +0530

    changes2

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/parials/header/header.component';
+import { HomeComponent } from './component/pages/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
